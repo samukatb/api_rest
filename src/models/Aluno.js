7153b62567aa2c23cb7f0a139826908884755870
samuka321 <samukatb2010@hotmail.com>
2021-12-19 06:38:04 -0300

    Rota de alunos pronta

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -3,12 +3,72 @@ import Sequelize, { Model } from 'sequelize';
 export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      peso: Sequelize.FLOAT,
-      altura: Sequelize.FLOAT,
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [2, 255],
+            msg: 'Nome precisa ter entre 3 e 255 caraceteres.',
+          },
+        },
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [2, 255],
+            msg: 'Sobrenome precisa ter entre 3 e 255 caraceteres.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'Email inválido',
+          },
+          isUnique(value, next) {
+            Aluno.findOne({ where: { email: value } })
+              .then((user) => {
+                if (user) {
+                  return next('E-mail já existe, tente outro!');
+                }
+                return next();
+              })
+              .catch((err) => next(err));
+          },
+        },
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade precisa ser um número inteiro',
+          },
+        },
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Peso precisa ser um número inteiro ou de ponto flutuante..',
+          },
+        },
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Altura precisa ser um número inteiro ou de ponto flutuante..',
+          },
+        },
+      },
     }, {
       sequelize,
     });
